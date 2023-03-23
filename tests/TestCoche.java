8d6965c40bfc6936e644485c9d7587844bd25868
Ivan Agullo <ivanagullosaez6@gmail.com>
2023-03-23 15:27:07 +0100

    Creada la rama Refactorizado y refactorizado los metodos y test

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,21 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_Ivan(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_Ivan(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_Ivan(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye_Ivan(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_Ivan(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
